8af0ebc9ff891d2c86ac9997aa3e07fcf4a5e698
Daksh299 <ddahiya42005@gmail.com>
2025-10-27 10:43:09 +0530

    Update a5_add2.cpp

--- a5_add2.cpp
+++ a5_add2.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// Definition for singly-linked list.
+
 struct ListNode {
     int val;
     ListNode* next;
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
-// Helper function to reverse k nodes of the linked list
+
 ListNode* reverseKNodes(ListNode* head, int k) {
     ListNode* prev = nullptr;
     ListNode* curr = head;
     ListNode* next = nullptr;
     int count = 0;
 
-    // Reverse k nodes or until the end of the list
+    
     while (curr != nullptr && count < k) {
         next = curr->next;
         curr->next = prev;
@@ -24,16 +24,15 @@ ListNode* reverseKNodes(ListNode* head, int k) {
         count++;
     }
 
-    // prev is the new head of the reversed group
+    
     return prev;
 }
 
-// Main function to reverse nodes in k groups
+
 ListNode* reverseKGroup(ListNode* head, int k) {
     if (!head || k == 1) return head;
 
-    // Check if there are at least k nodes left in the list
-    ListNode* node = head;
+   
     int count = 0;
     while (node != nullptr && count < k) {
         node = node->next;
@@ -41,21 +40,20 @@ ListNode* reverseKGroup(ListNode* head, int k) {
     }
 
     if (count < k) {
-        // Less than k nodes remaining, return head as is
+       
         return head;
     }
 
-    // Reverse first k nodes
+   
     ListNode* newHead = reverseKNodes(head, k);
 
-    // Now head is the last node of the reversed group
-    // Recurse for the rest of the list and connect
+    
     head->next = reverseKGroup(node, k);
 
     return newHead;
 }
 
-// Utility function to print linked list
+
 void printList(ListNode* head) {
     while (head) {
         cout << head->val << " ";
@@ -64,9 +62,9 @@ void printList(ListNode* head) {
     cout << "\n";
 }
 
-// Example usage
+
 int main() {
-    // Create linked list 1->2->3->4->5
+   
     ListNode* head = new ListNode(1);
     head->next = new ListNode(2);
     head->next->next = new ListNode(3);
